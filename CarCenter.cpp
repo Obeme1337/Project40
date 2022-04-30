3c06566540f52b66bb3640df24d51620d75dad3c
Obeme1337 <sashkov206@gmail.com>
2022-04-30 12:50:14 +0300

    Add CarCenter class

--- CarCenter.cpp
+++ CarCenter.cpp
@@ -1,1 +1,63 @@
 #include "CarCenter.h"
+
+
+CarCenter::CarCenter() {
+	brand = "no brand";
+	price = 10000;
+	colour = "red";
+	release = 2010;
+}
+
+CarCenter::CarCenter(string brand, double price, string colour, int release) {
+	this->brand = brand;
+	this->price = price;
+	this->colour = colour;
+	this->release = release;
+}
+
+
+string CarCenter::getBrand() {
+	return brand;
+}
+
+void CarCenter::setBrand(string brand) {
+	this->brand = brand;
+}
+
+double CarCenter::getPrice() {
+	return price;
+}
+
+void CarCenter::setPrice(double price) {
+	if (price >= MIN_PRICE && price <= MAX_PRICE) {
+		this->price = price;
+
+	}
+}
+
+
+string CarCenter::getColour() {
+	return colour;
+}
+
+void CarCenter::setColour(string colour) {
+	this->colour = colour;
+
+}
+
+int CarCenter::getRelease() {
+	return release;
+}
+
+void CarCenter::setRelease(int release) {
+	if (release >= MIN_YEAR_RELEASE && release <= MAX_YEAR_RELEASE) {
+		this->release = release;
+	}
+}
+
+
+string CarCenter::getInfo() {
+	return brand + ": price = " + to_string(price)
+		+ "; colour = " + colour
+		+ "; release = " + to_string(release);
+}
--- CarCenter.h
+++ CarCenter.h
@@ -1,7 +1,47 @@
 #pragma once
 #include "Header.h"
-
 class CarCenter
 {
+private:
+	string brand;
+	double price;
+	string colour;
+	int release;
+
+
+public:
+	static const int MAX_PRICE = 1000000;
+	static const int MIN_PRICE = 10000;
+
+	static const int MIN_YEAR_RELEASE = 2010;
+	static const int MAX_YEAR_RELEASE = 2022;
+
+
+
+	CarCenter();
+
+	CarCenter(string brand, double price, string colour, int release);
+
+	~CarCenter();
+
+	string getBrand();
+
+	void setBrand(string brand);
+
+	double getPrice();
+
+	void setPrice(double price);
+
+	string getColour();
+
+	void setColour(string colour);
+
+	int getRelease();
+
+
+	void setRelease(int release);
+
+
+	string getInfo();
 };
 
