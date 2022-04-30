14c621ff386c2c426ecd567b38be4a5eb7b5fb19
Obeme1337 <sashkov206@gmail.com>
2022-04-30 15:07:34 +0300

    Testing CarCenter class

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,35 @@
 #include "Car.h"
-
+#include "CarCenter.h"
 
 int main() {
 
-	Car car1("Ferrari", "V8", 100000, "red", 2020);
+	/*Car car1("Ferrari", "V8", 100000, "red", 2020);
 
-	cout << car1.getInfo() << endl;
+	cout << car1.getInfo() << endl;*/
+
+	CarCenter CarCenter1("Sausage");
+	CarCenter CarCenter2("Banana");
+
+
+	Car c1("Ferrari", "V8", 100000, "red", 2020);
+	Car c2("BMW", "X5", 50000, "blue", 2021);
+	Car c3("Mercedes", "E-class", 50000, "grey", 2022);
+
+	Car c4("Lamborghini", "Huracan", 100000, "yellow", 2019);
+	Car c5("Mercedes", "C-class", 50000, "white", 2021);
+	Car c6("Tesla", "Model X", 30000, "grey", 2022);
+
+
+	CarCenter1.add(c1);
+	CarCenter1.add(c2);
+	CarCenter1.add(c3);
+
+	CarCenter2.add(c4);
+	CarCenter2.add(c5);
+	CarCenter2.add(c6);
+
+	cout << CarCenter1.getInfo() << endl;
+	cout << CarCenter2.getInfo() << endl;
 
 	return 0;
 }
